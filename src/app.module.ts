272d042c048c8fac255d44f59ceeffde1782c5a6
Nick (Shark) Marino <nickmarino284@gmail.com>
2024-11-24 23:51:00 -0600

    Fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as fs from 'fs';
 
 @Module({
   imports: [
@@ -12,15 +13,16 @@ import { AppService } from './app.service';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,  // Disable in production
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false,
       extra: {
         ssl: {
-          rejectUnauthorized: false,
+          ca: fs.readFileSync('certs/DigiCertGlobalRootCA.crt.pem'), // Use the downloaded CA certificate
+          rejectUnauthorized: true,
         },
       },
     }),
