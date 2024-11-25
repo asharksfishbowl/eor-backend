ab52f3af3ec6ab73d1cab6786b00afca1360b28d
Nick (Shark) Marino <nickmarino284@gmail.com>
2024-11-24 20:44:48 -0600

    Fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'eor-backend',  // Give a name to your app instance
-            script: './main.js',  // Replace with your main entry point file (e.g., server.js or app.js)
+            script: 'dist/main.js',  // Replace with your main entry point file (e.g., server.js or app.js)
             instances: 1,  // Number of instances to be run. "max" uses all available CPUs
             autorestart: true,
             watch: false,  // Use true if you want PM2 to watch for file changes (dev only)
