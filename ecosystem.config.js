332b56a5e7728193cdddfc02d2354eb41654ef21
Nick (Shark) Marino <asharksfishbowl@gmail.com>
2024-12-28 12:25:35 -0600

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,17 @@
 module.exports = {
     apps: [
         {
-            name: 'backend-server',
-            script: '/home/site/wwwroot/main.js', // Use `next start` script to start the Next.js application
-            args: 'start',  // Pass 'start' to run the Next.js app in production mode
+            name: 'eor-backend',
+            script: 'node_modules/.bin/next',
+            args: 'start',
+            cwd: '/home/site/wwwroot', // The folder with package.json / .next
             instances: 1,
             autorestart: true,
             watch: false,
             max_memory_restart: '1G',
             env: {
                 NODE_ENV: 'production',
-                PORT: 3000, // Replace with your actual port
+                PORT: 3000,
             },
         },
     ],
